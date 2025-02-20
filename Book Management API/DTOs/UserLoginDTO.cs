2c066a75b4721191cdaa36013e0b6c030ee3c5c8
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-02-21 00:04:49 +0400

    added new DTOs

---
+++ Book Management API/DTOs/UserLoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Management_API.DTOs
+{
+    public class UserLoginDTO
+    {
+        [Required,EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Book Management API/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Management_API.DTOs
+{
+    public class UserRegisterDTO
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required, Compare("Password", ErrorMessage = "Passwords do not match"))]
+        public string ConfirmPassword { get; set; }
+    }
+}
