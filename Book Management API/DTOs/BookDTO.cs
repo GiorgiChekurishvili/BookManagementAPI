b486c5260ac5c8d5ae458721929191857b0e299a
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-02-20 19:10:38 +0400

    Added DTO and AutoMapper

---
+++ Book Management API/DTOs/BookDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Book_Management_API.DTOs
+{
+    public class BookDTO
+    {
+        public required string Title { get; set; }
+        public int PublicationYear { get; set; }
+        public required string AuthorName { get; set; }
+    }
+}
---
+++ Book Management API/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Book_Management_API.DTOs;
+using Book_Management_API.Models;
+
+namespace Book_Management_API
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Book, BookDTO>().ReverseMap();
+        }
+    }
+}
